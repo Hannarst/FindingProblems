3ea57f76a78d266c790cd5e8ae52716b759fa12e
Anna Borysova <annasborysova@gmail.com>
2016-08-08 14:24:14 +0200

    set up of project, and models

---
+++ problems/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Problem(models.Model):
+    DIFFICULTIES = zip(range(5), ['Very Easy', 'Easy', 'Average', 'Difficult', 'Very Difficult'])
+    title = models.TextField(max_length=200)
+    content = models.TextField()
+    difficulty = models.IntegerField(choices=DIFFICULTIES)
+    execution_time_limit = models.FloatField()
+    solution = models.TextField()
+    private = models.BooleanField(default=True)
+
+class Challenge(models.Model):
+    datetime = models.DateTimeField()
+    name = models.TextField(max_length=200)
+    problems = models.ManyToManyField(Problem)
+
+class Category(models.Model):
+    problems = models.ManyToManyField(Problem)
+    name = models.TextField(max_length=200)
